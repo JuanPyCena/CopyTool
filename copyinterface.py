a7c171dc57412c6967ab7cf91894122356a07282
Felix Sonnleitner <SunnyMOD@Felixs-MacBook-Pro-114.local>
2018-01-08 18:20:59 +0100

    added all files initially

---
+++ copyinterface.py
@@ -0,0 +1,130 @@
+# Felix Sonnleitner, 25.12.2017
+# Python 3.6
+# Module used to copy files from a device to a location
+
+import os
+from logmod import Logger
+from shutil import copy2
+from subprocess import PIPE, Popen
+
+class CopyInterface:
+    """This class is used to create an interface between the USB devices and the PC"""
+
+    def __init__(self, connector=None, device=None, destination=None, delete_after_copy=False):
+        self.log       = Logger()
+        self.connector = connector
+        self.device    = device
+        self.HD        = self.connector.get_HD()
+
+        self.usb_directories   = None
+        self.src               = str(self.device) + "/DCIM/"
+        self.destination       = destination
+        self.delete_after_copy = delete_after_copy
+
+        self.list = self.get_list_of_files_in_src(self.src)
+
+    #################################################################################
+
+    def copy_file_from_usb_to_HD(self):
+        for l in self.list:
+            try:
+                src = str(self.src) + l
+                if os.path.isfile(src):
+                    copy2(src, self.destination)
+                    self.log.write_to_log("SUCCESS: Copying " + l + " to " + self.destination)
+                else:
+                    self.log.write_to_log("INFO: {l} was not copied. {l} is a directory".format(l=src))
+            except:
+                self.log.write_to_log("ERROR: Could not copy " + l + " to " + self.destination)
+                return False
+
+        return True
+
+    #################################################################################
+
+    def remove_files_from_source(self):
+        for l in self.list:
+            try:
+                src = str(self.src) + l
+                if os.path.isfile(src):
+                    os.remove(src)
+                    self.log.write_to_log("SUCCESS: Removed " + l + " from USB device")
+                else:
+                    self.log.write_to_log("INFO: {l} was not removed. {l} is a directory".format(l=src))
+            except:
+                self.log.write_to_log("ERROR: Could not remove " + l + " from USB device")
+                return False
+
+        return True
+
+    #################################################################################
+
+    def destination_directory_exists(self, dir="/Volumes/Macintosh HD/Users/SunnyMOD/Pictures/destination"):
+        """
+        :param dir: string
+                    Defines the directory where the files should be copied to
+                    Default: ~/Pictures/destination
+        :return: bool
+                    Returns True if the directory already exists. Otherwise it tries to create the directory.
+                    If this fails False is returned.
+        """
+
+        if os.path.exists(dir):
+            return True
+
+        try:
+            os.mkdir(dir)
+            self.log.write_to_log("INFO: " + dir + " not found. Creating the directory to copy to")
+            return True
+        except:
+            self.log.write_to_log("ERROR: Could not create directory " + dir)
+            return False
+
+    #################################################################################
+
+    def get_list_of_files_in_src(self, src):
+        """
+        :param src: string
+                    The absoute path of the source directory
+        :return: list of string
+                    Return all elements in the source path as a list of string
+        """
+
+        list = []
+
+        ls = self._cmdline("ls {src}".format(src=src))
+
+        word = ""
+        for l in ls:
+            if l == "\n":
+                list.append(word)
+                word = ""
+            else:
+                word = word + l
+
+        self.log.write_to_log("--------------------------BEGIN: LIST TO COPY--------------------------")
+
+        for l in list:
+            self.log.write_to_log(l, timestamp=False)
+
+        self.log.write_to_log("---------------------------END: LIST TO COPY---------------------------")
+
+        return list
+
+    #################################################################################
+
+    def _cmdline(self, command):
+        """
+        :param command: string
+                    The console command which should be executed
+        :return: string utf-8
+                    The console output of the command
+        """
+        process = Popen(
+            args=command,
+            stdout=PIPE,
+            shell=True
+        )
+        return process.communicate()[0].decode("utf-8")
+
+#EOF
---
+++ gui.py
@@ -0,0 +1,115 @@
+# Felix Sonnleitner, 27.12.2017
+# Python 3.6
+# This module is used to create and use a small dialog window
+
+from guizero import App, Text, TextBox, CheckBox, PushButton, info, warn
+from logmod import Logger
+
+class GUI:
+
+    def __init__(self, info=False):
+        """
+        :param info: bool
+                    Defines if an additional Infobox is shown
+        """
+        self.log           = Logger()
+        self.info          = info
+        self.dir           = ""
+        self.delete_on_usb = False
+
+        self.app = None
+        self.main_window()
+
+    #################################################################################
+
+    def main_window(self):
+        self.app = App(title="Bilder kopieren", width=800, height=300)
+        welcome = Text(self.app, "Wilkommen in der Bilder Kopier App.", size=20)
+        enter = Text(self.app, "Gib bitte im Feld unten den Namen des Ordners ein in dem du deine Bilder kopieren möchtest. "
+                          "\n (Wenn es diesen Ordner noch nicht gibt wird er für dich unter \"Bilder\" erstellt) "
+                          "\n"
+                          "\nDanach wähle aus ob die Bilder von dem USB-Stick gelöscht werden sollen. "
+                          "\nWenn du die Bilder vom USB Stick löschen möchtest mach ein Häckchen bei: "
+                          "\n    \"Bilder nach dem Kopieren löschen...?\"     "
+                          "\n"
+                          "\nBestätige anschließend  deine Eingabe mit den Knopf BESTÄTIGEN", size=18)
+
+        dir = TextBox(self.app, width=80)
+        checkbox = CheckBox(self.app, text="Bilder nach dem Kopieren löschen...?")
+        OK = PushButton(self.app, text="BESTÄTIGEN", command=info,
+                        args=["INFO", "Die Bilder werden kopiert. \nDu kannst nun das Fenster schließen"])
+
+        if self.info:
+            inf = info("INFORMATION", "Gib bitte im Feld unten den Namen deines Ordners ein. "
+                                      "\n"
+                                      "\nDanach wähle aus ob die Bilder von dem USB-Stick gelöscht werden sollen. "
+                                      "\nWenn du die Bilder vom USB Stick löschen möchtest mach ein Häckchen bei: "
+                                      "\n    \"Bilder nach dem Kopieren löschen...?\"     "
+                                      "\n"
+                                      "\nBestätige anschließend deine Eingabe mit den Knopf \"BESTÄTIGEN\"", size=18)
+
+        self.app.focus()
+        self.app.display()
+
+        self.dir = dir.get()
+        self.log.write_to_log("INFO: The directory in which the files should be copied to is " + self.dir,
+                              consoleOutput=True)
+
+        if checkbox.get_value() == 1:
+            self.log.write_to_log("INFO: The copied files will be removed from the usb device", consoleOutput=True)
+            self.delete_on_usb = True
+
+
+        try:
+            pass
+
+        except:
+            self.log.write_to_log("ERROR: The main window has failed", consoleOutput=True)
+
+    #################################################################################
+
+    def destroy(self):
+        self.app.destroy()
+        self.app = None
+
+    #################################################################################
+
+    def finished_window(self):
+        info(title="Bilder kopieren fertig!!", text="Alle Bilder wurden nach {destination} kopiert.".format(destination=self.dir))
+
+    #################################################################################
+
+    def error_window(self):
+        warn(title="Bilder kopieren fehlgeschlagen!!", text="Bilder wurden nicht kopiert!!!".format(destination=self.dir))
+
+    #################################################################################
+
+    def finished_removing_window(self):
+        info(title="Bilder löschen fertig!!",
+             text="Alle Bilder wurden vom USB Stick gelöscht.")
+
+    #################################################################################
+
+    def error_removing_window(self):
+        warn(title="Bilder löschen fehlgeschlagen!!",
+             text="Bilder wurden nicht vom USB Stick gelöscht!!!")
+
+    #################################################################################
+
+    def get_dir(self):
+        """
+        :return: string
+                    The entered name for the Directory
+        """
+        return self.dir
+
+    #################################################################################
+
+    def get_delete_on_usb(self):
+        """
+        :return: bool
+                    Defines if the files should be deleted on the USB device
+        """
+        return self.delete_on_usb
+
+#EOF
